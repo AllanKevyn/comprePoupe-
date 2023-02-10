e94198e9c33ef35d5c4ba6ccad8420a638240e22
Marllon Silibri <marllonmunhoz321@hotmail.com>
2023-02-10 20:22:07 +0000

    Adicionando nav. resource

--- app/src/main/java/com/example/comprepoupe/presentation/activitys/MainActivity.kt
+++ app/src/main/java/com/example/comprepoupe/presentation/activitys/MainActivity.kt
@@ -1,12 +1,12 @@
-package com.example.comprepoupe
+package com.example.comprepoupe.presentation.activitys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.comprepoupe.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //askjbasc
     }
 }
